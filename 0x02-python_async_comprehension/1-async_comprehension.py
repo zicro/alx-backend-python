744621ec0653e1e8a87e721ee5d0ad1c23d385cc
xLion <larhnimikit@gmail.com>
2024-03-19 09:07:39 +0000

    python async comprehension..

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return 10 random numbers"""
+    return [rand async for rand in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Run time for parallel"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Return runtime"""
+    start = time.time()
+    await asyncio.gather(*[async_comprehension() for r in range(4)])
+    return time.time() - start
